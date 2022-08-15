520bfb6d77bdf8fb2c15e92636d509405dc5894d
Chad Reynolds <chadrey0108@icloud.com>
2022-08-15 16:27:55 -0400

    added change.js file

---
+++ change.js
@@ -0,0 +1,7 @@
+function totalChange(amountPaid, totalCost) {
+    console.log(parseFloat(amountPaid % totalCost).toFixed(2));
+  }
+  
+  // ---- TESTS - don't change these ---- //
+  console.log(totalChange(100, 75) === 25)
+  console.log(totalChange(10, 7.28) === 2.72)
