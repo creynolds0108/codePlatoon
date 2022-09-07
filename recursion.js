b027fb994acd4fca84f23e51972d044b3aea0c96
Chad Reynolds <107443854+creynolds0108@users.noreply.github.com>
2022-09-07 18:23:52 -0400

    Update recursion.js

--- recursion.js
+++ recursion.js
@@ -1,11 +1,11 @@
 function sum(arr, n) {
-    // Only change code below this line
-    if (n <= 0) {
-        return 1
-    } else {
-        return arr(arr[0], n - 1) + arr[n - 1]
-    }
-    // Only change code above this line
+  // Only change code below this line
+  if (n <= 0) {
+    return 1;
+  } else {
+    return arr(arr[0], n - 1) + arr[n - 1];
+  }
+  // Only change code above this line
 }
 
-sum([1], 0)
+sum([1], 0);
